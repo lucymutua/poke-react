2a8d0e3c4e04efecf50d7d0c0eb893c6296c3a92
Usuario <you@example.com>
2023-05-17 14:57:57 +0200

    SearchPage.jsx

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -5,24 +5,38 @@ import { PokemonContext } from '../context/PokemonContext';
 
 export const SearchPage = () => {
 	const location = useLocation();
+	console.log(location);
 
 	const { globalPokemons } = useContext(PokemonContext);
 
-	const filteredPokemons = globalPokemons.filter(pokemon =>
-		pokemon.name.includes(location.state.toLowerCase())
-	);
+	let filteredPokemons = null
+
+  if(location.state!=null){
+   filteredPokemons = globalPokemons.filter(pokemon => 
+	pokemon.name.includes(location.state.toLowerCase()))
+  console.log("entra1")
+  }
 
 	return (
-		<div className='container'>
-			<p className='p-search'>
-				Se encontraron <span>{filteredPokemons.length}</span>{' '}
-				resultados:
-			</p>
-			<div className='card-list-pokemon container'>
-				{filteredPokemons.map(pokemon => (
-					<CardPokemon pokemon={pokemon} key={pokemon.id} />
-				))}
+
+		location.state ? (
+			<div className="container">
+				<p className="p-search">
+					Se encontraron <span>{filteredPokemons.length}</span>{' '}resultados:
+				</p>
+				<div className="card-list-pokemon container">
+					{filteredPokemons.map(pokemon => <CardPokemon pokemon={pokemon} key={pokemon.id} />)}
+				</div>
 			</div>
-		</div>
-	);
+
+		) : (
+
+
+			<p className="p-search">
+				Se encontraron <span>0</span>{' '}resultados:
+			</p>
+
+
+		)
+	)
 };
